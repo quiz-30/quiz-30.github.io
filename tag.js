c59fc79e8fdb9c8a5902de07f6f30352683e8483
class-80x <cansuakcay12@gmail.com>
2025-01-14 20:56:54 +0300

    sd

--- tag.js
+++ tag.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const tabTitle = document.getElementById("tab-title");
   const description = document.getElementById("description");
   title.innerHTML = tag.replace(/-/g, " ");
-  tabTitle.innerHTML = tag.replace(/-/g, " ") + " | quiz-30.github.io";
+  tabTitle.innerHTML = tag.replace(/-/g, " ") + " | MacVG";
   switch (title.innerHTML) {
     case "RPG Games":
       description.innerHTML =
